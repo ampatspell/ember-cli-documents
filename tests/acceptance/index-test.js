929d07de9a89ab5456f5edb506a852f0eaed69df
ampatspell <ampatspell@gmail.com>
2017-09-12 19:49:43 +0300

    dummy

---
+++ tests/acceptance/index-test.js
@@ -0,0 +1,9 @@
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | index');
+
+test('visiting /', function(assert) {
+  visit('/');
+  andThen(() => assert.equal(currentURL(), '/'));
+});
