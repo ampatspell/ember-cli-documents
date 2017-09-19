c41a4f1c3e1ebdbb36ea7fce1d4bf60cf85f3d60
ampatspell <ampatspell@gmail.com>
2017-09-19 22:51:15 +0300

    lazy create stores, store, db in module-for-db

--- tests/helpers/module-for-db.js
+++ tests/helpers/module-for-db.js
@@ -7,14 +7,16 @@ const {
   RSVP: { resolve }
 } = Ember;
 
+const getter = (object, name, fn) => Object.defineProperty(object, name, { get: () => fn() });
+
 export default function(name, options = {}) {
   module(name, {
     beforeEach() {
       this.application = startApp();
       this.instance = this.application.buildInstance();
-      this.stores = this.instance.lookup('documents:stores');
-      this.store = this.stores.store({ url: 'http://127.0.0.1:5984' });
-      this.db = this.store.database('thing');
+      getter(this, 'stores', () => this.instance.lookup('documents:stores'));
+      getter(this, 'store', () => this.stores.store({ url: 'http://127.0.0.1:5984' }));
+      getter(this, 'db', () => this.store.database('thing'));
       if (options.beforeEach) {
         return options.beforeEach.apply(this, arguments);
       }
