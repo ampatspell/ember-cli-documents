f4723b443086584b9efea23ac346b22a4a36aedc
ampatspell <ampatspell@gmail.com>
2017-09-26 17:41:31 +0300

    remove experiments

--- experiments/acceptance/index-test.js
+++
@@ -1,9 +0,0 @@
-import module from '../../tests/helpers/module-for-acceptance';
-import { test } from '../helpers/qunit';
-
-module('Acceptance | index');
-
-test.skip('visiting /', function(assert) {
-  visit('/');
-  andThen(() => assert.equal(currentURL(), '/'));
-});
