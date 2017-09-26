2dc6939abb9cc7df11c4669e499723228e75cd06
ampatspell <ampatspell@gmail.com>
2017-09-26 17:41:25 +0300

    few more array tests from experiments

--- tests/unit/document-array-test.js
+++ tests/unit/document-array-test.js
@@ -182,3 +182,118 @@ test('replace array contents removes all content (needs fixing)', function(asser
   let second = doc.get('names.lastObject');
   assert.ok(first._internal !== second._internal);
 });
+
+test('set array with objects', function(assert) {
+  let doc = this.db.doc({
+    id: 'duck:yellow',
+    stuff: [
+      { city: 'Yello', country: 'Duckland' },
+      'hey',
+      [
+        { ok: true }
+      ]
+    ]
+  });
+  assert.deepEqual(doc.get('serialized'), {
+    id: 'duck:yellow',
+    stuff: [
+      { city: 'Yello', country: 'Duckland' },
+      'hey',
+      [
+        { ok: true }
+      ]
+    ]
+  });
+});
+
+test('replace array', function(assert) {
+  let doc = this.db.doc({
+    id: 'thing',
+    things: [
+      { id: 'a' },
+      { id: 'b' },
+    ]
+  });
+
+  assert.deepEqual(doc.serialize(), {
+    "id": "thing",
+    "things": [
+      { id: 'a' },
+      { id: 'b' },
+    ]
+  });
+
+  assert.deepEqual(doc.get('serialized'), {
+    "id": "thing",
+    "things": [
+      { id: 'a' },
+      { id: 'b' },
+    ]
+  });
+
+  this.db._deserializeSavedInternal(doc._internal, {
+    _id: 'thing',
+    things: [
+      { id: 'c' },
+      { id: 'a' }
+    ]
+  });
+
+  assert.deepEqual(doc.serialize(), {
+    "id": "thing",
+    "things": [
+      { id: 'c' },
+      { id: 'a' }
+    ]
+  });
+
+  assert.deepEqual(doc.get('serialized'), {
+    "id": "thing",
+    "things": [
+      { id: 'c' },
+      { id: 'a' }
+    ]
+  });
+});
+
+test('create array and attach to document', function(assert) {
+  let array = this.db.array([ { ok: true } ]);
+  assert.ok(array._internal);
+  assert.ok(!array._internal.parent);
+  assert.equal(array.get('length'), 1);
+  let ok = array.objectAt(0);
+
+  let doc = this.db.doc();
+  doc.set('array', array);
+  assert.ok(doc.get('array')._internal.parent === doc._internal);
+  assert.ok(doc.get('array') === array);
+  assert.ok(doc.get('array.lastObject') === ok);
+});
+
+test('create object and attach to array', function(assert) {
+  let doc = this.db.doc();
+  let array = this.db.array();
+  let ok = this.db.object({ ok: true });
+
+  array.pushObject(ok);
+  assert.ok(array.objectAt(0)._internal === ok._internal);
+  assert.ok(ok._internal.parent === array._internal);
+
+  doc.set('array', array);
+  assert.ok(array._internal.parent === doc._internal);
+
+  doc.set('object', ok);
+  assert.ok(doc.get('object') !== ok);
+  assert.ok(doc.get('object')._internal !== ok._internal);
+
+  assert.deepEqual_(doc.get('serialized'), {
+    "array": [
+      {
+        "ok": true
+      }
+    ],
+    "object": {
+      "ok": true
+    }
+  });
+});
