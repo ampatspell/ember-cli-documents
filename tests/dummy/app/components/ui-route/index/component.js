d6dc62b05c43b0073ae38c47b3f2d966c8e4b9a4
ampatspell <ampatspell@gmail.com>
2017-09-29 01:29:21 +0300

    ugly login, logout in index route

--- tests/dummy/app/components/ui-route/index/component.js
+++ tests/dummy/app/components/ui-route/index/component.js
@@ -2,12 +2,15 @@ import Ember from 'ember';
 import layout from './template';
 
 const {
-  computed
+  computed,
+  computed: { reads }
 } = Ember;
 
 export default Ember.Component.extend({
   classNameBindings: [ ':ui-route', ':index' ],
   layout,
+
+  session: reads('store.session'),
 
   author: computed(function() {
     return window.author;
