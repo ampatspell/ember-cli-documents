e305b9218fcd3842c6ca55f574656a8984998017
ampatspell <ampatspell@gmail.com>
2017-09-30 12:46:40 +0300

    internal attachments extends Base

--- addon/document/attachments/internal/attachments.js
+++ addon/document/attachments/internal/attachments.js
@@ -1,27 +1,16 @@
-export default class Attachments {
+import Base from '../../internal/base';
+
+export default class Attachments extends Base {
 
   constructor(store, document) {
-    this.store = store;
-    this.document = document;
-    this._model = null;
+    super(store, document);
   }
 
   _createModel() {
     return this.store._createAttachmentsModel(this);
   }
 
-  model(create) {
-    let model = this._model;
-    if(!model && create) {
-      model = this._createModel();
-      this._model = model;
-    }
-    return model;
-  }
-
-  _modelWillDestroy() {
-    let model = this._model;
-    this._model = null;
+  _deserialize() {
   }
 
 }
