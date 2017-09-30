e672d3df1fcae1bac418c86a656dae611c54813d
ampatspell <ampatspell@gmail.com>
2017-10-01 00:15:35 +0300

    register changes models in openChanges

--- addon/database/changes.js
+++ addon/database/changes.js
@@ -1,9 +1,22 @@
 import Ember from 'ember';
+import { array } from '../util/computed';
+import { destroyArray } from '../util/destroy';
 
 export default Ember.Mixin.create({
 
+  openChanges: array(),
+
   changes(opts) {
-    return {};
+    let changes = {
+      destroy() {}
+    };
+    this.get('openChanges').pushObject(changes);
+    return changes;
+  },
+
+  willDestroy() {
+    destroyArray(this.get('openChanges'));
+    this._super();
   }
 
 });
--- tests/unit/database-changes-test.js
+++ tests/unit/database-changes-test.js
@@ -1,5 +1,10 @@
+import Ember from 'ember';
 import configurations from '../helpers/configurations';
 import { test } from '../helpers/qunit';
+
+const {
+  run
+} = Ember;
 
 configurations(module => {
 
@@ -9,9 +14,26 @@ configurations(module => {
     }
   });
 
-  test('changes feed without view', function(assert) {
-    let changes = this.db.changes({ feed: this.config.feed, view: null });
-    assert.ok(changes);
+  /*
+  let changes = this.docs.createChanges({ feed: 'event-source' });
+  changes.on('data', arg => {
+    console.log(arg);
+  });
+  changes.start();
+  */
+
+  test('changes are registered', function(assert) {
+    let changes = this.db.changes({ feed: this.config.feed });
+    assert.ok(this.db.get('openChanges').includes(changes));
+  });
+
+  test('changes are destroyed with db', function(assert) {
+    let changes = this.db.changes({ feed: this.config.feed });
+    assert.ok(this.db.get('openChanges').includes(changes));
+
+    run(() => this.db.destroy());
+
+    assert.ok(!this.db.get('openChanges').includes(changes));
   });
 
 });
