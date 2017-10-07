87306d56084fd3c2252f645001fab3c543bbc89b
ampatspell <ampatspell@gmail.com>
2017-10-07 17:35:51 +0300

    remove extra state keys if defaults doesn't include them

--- addon/document/internal/-create-state.js
+++ addon/document/internal/-create-state.js
@@ -1,11 +1,70 @@
 import Ember from 'ember';
 
 const {
-  assign
+  assign,
+  A
 } = Ember;
 
 export default defaults => {
-  let keys = Object.keys(defaults);
+  let keys = A(Object.keys(defaults));
+  let create = hash => {
+    A(Object.keys(hash)).forEach(key => {
+      if(!keys.includes(key)) {
+        delete hash[key];
+      }
+    });
+    return hash;
+  };
+  let states = {
+    onDirty: create({
+      isDirty: true
+    }),
+    onLoading: create({
+      isLoading: true,
+      isError: false,
+      error: null
+    }),
+    onLoaded: create({
+      isNew: false,
+      isLoading: false,
+      isLoaded: true,
+      isDirty: false,
+      isSaving: false,
+      isDeleted: false,
+      isError: false,
+      error: null
+    }),
+    onSaving: create({
+      isSaving: true,
+      isError: false,
+      error: null
+    }),
+    onSaved: create({
+      isNew: false,
+      isLoading: false,
+      isLoaded: true,
+      isDirty: false,
+      isSaving: false,
+      isDeleted: false,
+      isError: false,
+      error: null
+    }),
+    onDeleted: create({
+      isNew: false,
+      isLoading: false,
+      isLoaded: true,
+      isDirty: false,
+      isSaving: false,
+      isDeleted: true,
+      isError: false,
+      error: null
+    }),
+    onError: create({
+      isLoading: false,
+      isSaving: false,
+      isError: true
+    })
+  };
 
   class State {
 
@@ -36,49 +95,23 @@ export default defaults => {
     }
 
     onDirty(changed) {
-      this.set({ isDirty: true }, changed);
+      this.set(states.onDirty, changed);
     }
 
     onLoading(changed) {
-      this.set({
-        isLoading: true,
-        isError: false,
-        error: null
-      }, changed);
+      this.set(states.onLoading, changed);
     }
 
     onLoaded(changed) {
-      this.set({
-        isNew: false,
-        isLoading: false,
-        isLoaded: true,
-        isDirty: false,
-        isSaving: false,
-        isDeleted: false,
-        isError: false,
-        error: null
-      }, changed);
+      this.set(states.onLoaded, changed);
     }
 
     onSaving(changed) {
-      this.set({
-        isSaving: true,
-        isError: false,
-        error: null
-      }, changed);
+      this.set(states.onSaving, changed);
     }
 
     onSaved(changed) {
-      this.set({
-        isNew: false,
-        isLoading: false,
-        isLoaded: true,
-        isDirty: false,
-        isSaving: false,
-        isDeleted: false,
-        isError: false,
-        error: null
-      }, changed);
+      this.set(states.onSaved, changed);
     }
 
     onDeleting(changed) {
@@ -86,25 +119,11 @@ export default defaults => {
     }
 
     onDeleted(changed) {
-      this.set({
-        isNew: false,
-        isLoading: false,
-        isLoaded: true,
-        isDirty: false,
-        isSaving: false,
-        isDeleted: true,
-        isError: false,
-        error: null
-      }, changed);
+      this.set(states.onDeleted, changed);
     }
 
     onError(error, changed) {
-      this.set({
-        isLoading: false,
-        isSaving: false,
-        isError: true,
-        error
-      }, changed);
+      this.set(assign({ error }, states.onError), changed);
     }
 
   }
