18dce1b33e1a15db976a54aceafc1c6bc227ca58
ampatspell <ampatspell@gmail.com>
2017-10-08 17:38:36 +0300

    slightly generalize computed property creation

--- tests/dummy/app/components/ui-route/experimental/index/component.js
+++ tests/dummy/app/components/ui-route/experimental/index/component.js
@@ -3,24 +3,58 @@ import layout from './template';
 
 const {
   computed,
-  merge
+  merge,
+  get
 } = Ember;
+
+const mapProperties = (owner, mapping) => {
+  let properties = {};
+  for(let key in mapping) {
+    properties[key] = get(owner, mapping[key]);
+  }
+  return properties;
+}
+
+const buildQuery = (owner, opts) => {
+  let properties = mapProperties(owner, get(opts, 'properties'));
+  return opts.query.call(owner, properties);
+}
+
+const lookupDatabase = (owner, opts) => {
+  return get(owner, get(opts, 'database'));
+}
+
+const createDocumentProxy = (database, query) => {
+  if(!database) {
+    return;
+  }
+  if(!query) {
+    return;
+  }
+  return database._createInternalDocumentProxy(query).model(true);
+};
+
+const first = opts => {
+  opts = merge({ database: 'database', properties: {} }, opts);
+  let keys = Object.values(opts.properties);
+  return computed(opts.database, ...keys, function() {
+    let database = lookupDatabase(this, opts);
+    let query = buildQuery(this, opts);
+    return createDocumentProxy(database, query);
+  }).readOnly();
+};
 
 const docById = opts => {
   opts = merge({ database: 'database', id: 'id' }, opts);
-  return computed(opts.database, opts.id, function() {
-    let database = this.get(opts.database);
-    if(!database) {
-      return;
+  let { database, id } = opts;
+  return first({
+    database,
+    properties: { id },
+    query(props) {
+      let { id } = props;
+      return { id };
     }
-    let id = this.get(opts.id);
-    if(!id) {
-      return;
-    }
-    // TODO: query & content lookup instead of `id`
-    let internal = database._createInternalDocumentProxy({ id });
-    return internal.model(true);
-  }).readOnly();
+  });
 };
 
 export default Ember.Component.extend({
