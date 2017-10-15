6314e7bc43f0c138f1c475d454afb67eeba1455a
ampatspell <ampatspell@gmail.com>
2017-10-15 19:06:59 +0300

    add didLoad in base proxy computed prop

--- addon/properties/proxy.js
+++ addon/properties/proxy.js
@@ -14,12 +14,12 @@ const query = () => {};
 const proxy = factory => opts => {
   opts = merge({ database: 'database', owner: [], document: [], matches, query }, opts);
   return computed(opts.database, function() {
-    let { query, matches } = opts;
+    let { query, matches, didLoad } = opts;
     let database = this.get(opts.database);
     let owner = copy(opts.owner);
     let document = copy(opts.document);
     assert(`Database not found for key ${opts.database}`, !!database);
-    let internal = factory(database, this, { owner, document, matches, query });
+    let internal = factory(database, this, { owner, document, matches, query, didLoad });
     registerDestroy(this, () => internal.destroy());
     return internal.model(true);
   }).readOnly();
