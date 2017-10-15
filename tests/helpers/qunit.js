237fa3666614aeff6ba23b7c7e55bc4b63f2ad1c
ampatspell <ampatspell@gmail.com>
2017-10-15 17:15:09 +0300

    test.todo

--- tests/helpers/qunit.js
+++ tests/helpers/qunit.js
@@ -3,6 +3,7 @@ import extendAssert from './extend-assert';
 import {
   test as test_,
   only as only_,
+  todo as todo_,
   skip
 } from 'ember-qunit';
 
@@ -24,6 +25,8 @@ const wrap = q => function(name, fn) {
 
 export const test = wrap(test_);
 export const only = wrap(only_);
+export const todo = wrap(todo_);
 
 test.skip = skip;
 test.only = only;
+test.todo = todo;
