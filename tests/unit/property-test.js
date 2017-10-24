954400c96e2776d467aa0cdd75837d4774f60462
ampatspell <ampatspell@gmail.com>
2017-10-24 03:59:11 +0300

    keep current internal/proxy in prefixed key, destroy on recompute

--- tests/unit/property-test.js
+++ tests/unit/property-test.js
@@ -39,3 +39,37 @@ test('property has definition meta', function(assert) {
   let instanceDef = getDefinition(owner, 'doc');
   assert.equal(instanceDef.database, 'db');
 });
+
+test('property destroys previous proxy on database change', function(assert) {
+  let Owner = Ember.Object.extend({
+    id: 'duck',
+    doc: docById({ database: 'db', id: 'id' })
+  });
+
+  let owner = Owner.create({ db: this.db });
+
+  let first = owner.get('doc');
+  owner.set('db', this.store.database('another'));
+  let second = run(() => owner.get('doc'));
+
+  assert.ok(second._internal);
+
+  assert.ok(first !== second);
+  assert.ok(first.isDestroying);
+});
+
+test('property does not recreate proxy on same database set', function(assert) {
+  let Owner = Ember.Object.extend({
+    id: 'duck',
+    doc: docById({ database: 'db', id: 'id' })
+  });
+
+  let owner = Owner.create({ db: this.db });
+
+  let first = owner.get('doc');
+  owner.set('db', this.db);
+  let second = owner.get('doc');
+
+  assert.ok(first === second);
+  assert.ok(!first.isDestroying);
+});
