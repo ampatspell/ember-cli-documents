d36701a2bfb6b807d3794ed50a5482e3a7296f54
ampatspell <ampatspell@gmail.com>
2017-10-26 05:08:07 +0300

    update ember-cli-couch dep

--- blueprints/ember-cli-documents/index.js
+++ blueprints/ember-cli-documents/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName: function() {
   },
   afterInstall: function() {
-    return this.addAddonToProject('ember-cli-couch', '1.0.10');
+    return this.addAddonToProject('ember-cli-couch', '1.0.11');
   }
 };
