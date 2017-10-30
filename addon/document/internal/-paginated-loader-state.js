14f5f7559ff251a55b995a0562885e8f910b4e78
ampatspell <ampatspell@gmail.com>
2017-10-30 22:19:01 +0200

    remove onReload. this is handled in -loader

--- addon/document/internal/-paginated-loader-state.js
+++ addon/document/internal/-paginated-loader-state.js
@@ -9,15 +9,6 @@ const extend = State => class PaginatedState extends stateMixin(State) {
     return this._loader._isMore;
   }
 
-  onReload(changed) {
-    this.set({
-      isLoading: false,
-      isLoaded: false,
-      isError: false,
-      error: null
-    }, changed);
-  }
-
 }
 
 const { keys, State } = createState({ defaults, computed, extend });
