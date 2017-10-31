8d06f705d20dd0ec8142c0674e1f6dd8dfa60e38
ampatspell <ampatspell@gmail.com>
2017-10-31 22:31:18 +0200

    add toStringExtenstion for store

--- addon/database/store.js
+++ addon/database/store.js
@@ -5,6 +5,10 @@ export default Ember.Mixin.create({
   store: null,
   identifier: null,
 
+  toStringExtension() {
+    return this.get('identifier');
+  },
+
   willDestroy() {
     this.get('store')._databaseWillDestroy(this);
     this._super();
