2846d91bb2f57ec0c0868bcc2377b8519e03faeb
ampatspell <ampatspell@gmail.com>
2017-10-31 16:59:35 +0200

    properties/-destroyable
    which caches and destroys property values

--- addon/properties/-destroyable.js
+++ addon/properties/-destroyable.js
@@ -1,20 +1,78 @@
 import Ember from 'ember';
-import { registerDestroy, destroyRegistered } from 'documents/util/register-destroy';
+import EmptyObject from 'documents/util/empty-object';
 
 const {
   computed
 } = Ember;
 
+const __ember_cli_documents__ = '__ember_cli_documents__';
+
+const _get = owner => {
+  return owner.willDestroy[__ember_cli_documents__];
+}
+
+const _cache = (owner, create) => {
+  if(!owner) {
+    return;
+  }
+  if(!owner.willDestroy) {
+    return;
+  }
+  let object = _get(owner);
+  if(!object && create) {
+    object = new EmptyObject();
+    let willDestroy = owner.willDestroy;
+    owner.willDestroy = function() {
+      let object = _get(owner);
+      for(let key in object) {
+        let value = object[key];
+        value.destroy();
+      }
+      willDestroy.apply(owner, arguments);
+    }
+    owner.willDestroy[__ember_cli_documents__] = object;
+  }
+  return object;
+}
+
+const _cacheFor = (owner, key) => {
+  let cache = _cache(owner, false);
+  if(!cache) {
+    return {};
+  }
+  return cache[key];
+}
+
+const _destroyCached = (owner, key, destroy) => {
+  let cache = _cache(owner);
+  delete cache[key];
+  destroy();
+}
+
+const _store = (owner, key, value, destroy) => {
+  let cache = _cache(owner, true);
+  cache[key] = { value, destroy };
+}
+
+export const cacheFor = (owner, key) => {
+  let cache = _cache(owner);
+  if(!cache) {
+    return;
+  }
+  let hash = cache[key];
+  if(!hash) {
+    return;
+  }
+  return hash.value;
+}
+
 export default (...args) => {
   let { create, get } = args.pop();
   return computed(...args, function(key) {
-    let cacheKey = `__ember_cli_documents_cached_${key}`;
-
-    let { value, destroy } = this[cacheKey] || {};
+    let { value, destroy } = _cacheFor(this, key);
 
     if(value) {
-      destroyRegistered(this, destroy);
-      delete this[cacheKey];
+      _destroyCached(this, key, destroy);
     }
 
     value = create.call(this, key);
@@ -23,13 +81,12 @@ export default (...args) => {
       return;
     }
 
-    destroy = registerDestroy(this, () => value.destroy());
+    _store(this, key, value, () => value.destroy());
 
-    this[cacheKey] = {
-      value,
-      destroy
-    };
+    if(get) {
+      return get.call(this, value, key);
+    }
 
-    return get.call(this, value, key);
+    return value;
   });
 }
