384a8c7d15b3444ac8e1f31292e59f0ccd0cbbfa
ampatspell <ampatspell@gmail.com>
2017-11-02 03:29:27 +0200

    declare extendable property special keys in properties/extendable

--- addon/properties/extendable.js
+++ addon/properties/extendable.js
@@ -1,10 +1,15 @@
-import extendable from './-extendable';
+import createExtendable from './-create-extendable';
 
 import {
   first as first_,
   find as find_,
   paginated as paginated_
 } from './proxy';
+
+let extendable = createExtendable({
+  arrays: [ 'owner', 'document' ],
+  functions: [ 'query', 'matches', 'loaded' ]
+});
 
 export const first     = extendable(first_);
 export const find      = extendable(find_);
