de3158055eddce6549c2ec12260635276d72440c
ampatspell <ampatspell@gmail.com>
2017-11-03 04:57:28 +0200

    dummy

--- tests/dummy/app/components/ui-route/blog/index/component.js
+++ tests/dummy/app/components/ui-route/blog/index/component.js
@@ -1,7 +1,19 @@
+import { readOnly } from '@ember/object/computed';
 import Component from '@ember/component';
 import layout from './template';
+import { info } from 'documents/util/logger';
 
 export default Component.extend({
   classNameBindings: [ ':ui-route', ':blog-index' ],
-  layout
+  layout,
+
+  blog: readOnly('state.blog'),
+
+  actions: {
+    select(name, object) {
+      window[name] = object;
+      info(`window.${name} = ${object}`);
+    }
+  }
+
 });
