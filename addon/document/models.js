03186deda56341717d4ffbf161171a5069815330
ampatspell <ampatspell@gmail.com>
2017-11-17 11:22:45 +0200

    add store to models

--- addon/document/models.js
+++ addon/document/models.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 import ModelMixin from './-model-mixin';
 import TransformMixin from './-array-transform-mixin';
+import { property } from './-properties';
+
+const store = property('store');
 
 export default Ember.ArrayProxy.extend(ModelMixin, TransformMixin, {
+
+  store: store()
+
 });
