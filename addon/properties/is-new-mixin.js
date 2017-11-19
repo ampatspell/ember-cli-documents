eea1ac62cebfcdb4fc57d80d53e70402b0158184
ampatspell <ampatspell@gmail.com>
2017-11-19 13:13:07 +0200

    comment out is-new-mixin

--- addon/properties/is-new-mixin.js
+++ addon/properties/is-new-mixin.js
@@ -1,30 +1,28 @@
 import Ember from 'ember';
-import { prop } from './index';
 
-const {
-  merge,
-  typeOf
-} = Ember;
+export default () => {
+  throw new Ember.Error('is-new-mixin');
+};
 
-export default extendable => extendable.extend(opts => {
-  opts = merge({ new: undefined }, opts);
-  opts.new = prop.wrap(opts.new);
-  return {
-    owner: [ opts.new.key() ],
-    document: [ 'isNew' ],
-    matches(doc, owner) {
-      if(this._super && !this._super(doc, owner)) {
-        return;
-      }
+// export default extendable => extendable.extend(opts => {
+//   opts = merge({ new: undefined }, opts);
+//   opts.new = prop.wrap(opts.new);
+//   return {
+//     owner: [ opts.new.key() ],
+//     document: [ 'isNew' ],
+//     matches(doc, owner) {
+//       if(this._super && !this._super(doc, owner)) {
+//         return;
+//       }
 
-      let value = opts.new.value(owner);
-      let type = typeOf(value);
+//       let value = opts.new.value(owner);
+//       let type = typeOf(value);
 
-      if(type === 'null' || type === 'undefined') {
-        return true;
-      }
+//       if(type === 'null' || type === 'undefined') {
+//         return true;
+//       }
 
-      return doc.get('isNew') === !!value;
-    }
-  };
-});
+//       return doc.get('isNew') === !!value;
+//     }
+//   };
+// });
