b1b910c02238f0a45c9c296e5f15badf82514388
ampatspell <ampatspell@gmail.com>
2017-11-19 21:03:42 +0200

    assert proxy database prop type

--- addon/properties/proxy/-create-proxy.js
+++ addon/properties/proxy/-create-proxy.js
@@ -2,9 +2,11 @@ import { merge } from '@ember/polyfills';
 import { omit } from '../../util/object';
 import destroyable from '../-destroyable';
 import { withDefinition } from '../-meta';
+import { isString } from '../../util/assert';
 
 export default type => opts => {
   opts = merge({ database: 'database' }, opts);
+  isString('database', opts.database);
   return withDefinition(destroyable(opts.database, {
     create() {
       let database = this.get(opts.database);
