ede3e4c830bee166d215dd8f53b9a3196bf2ceff
ampatspell <ampatspell@gmail.com>
2017-11-21 01:44:58 +0200

    paginated loader loaded result assert, tests

--- addon/document/internal/paginated-loader.js
+++ addon/document/internal/paginated-loader.js
@@ -1,6 +1,7 @@
 import { A } from '@ember/array';
 import Loader from './-loader';
 import PaginatedLoaderState from './-paginated-loader-state';
+import { isObject } from 'documents/util/assert';
 
 export default class PaginatedLoaderInternal extends Loader {
 
@@ -69,9 +70,12 @@ export default class PaginatedLoaderInternal extends Loader {
 
     let docs = A(result.map(internal => internal.model(true)));
 
-    let { state, isMore } = this.opts.loaded(query.state, docs);
+    let loaded = this.opts.loaded(query.state, docs);
+    isObject('loaded function result', loaded);
 
-    query.isMore = isMore;
+    let { state, isMore } = loaded;
+
+    query.isMore = !!isMore;
     query.state = state;
 
     this._withState((state, changed) => changed('isMore'));
