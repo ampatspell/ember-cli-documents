2bcfe692035c66756f452078f471e2e22d7954cc
ampatspell <ampatspell@gmail.com>
2017-11-23 01:50:14 +0200

    failing test for internal model creation refactoring

---
+++ tests/unit/internal-model-test.js
@@ -0,0 +1,31 @@
+import { assign } from '@ember/polyfills';
+import module from '../helpers/module-for-db';
+import { test } from '../helpers/qunit';
+import { Model } from 'documents';
+
+const Duck = Model.extend();
+
+module('internal-model', {
+  beforeEach() {
+    this.register('model:duck', Duck);
+    this.defaults = {
+      database: this.db,
+      type: 'duck',
+      props:  { ok: true },
+      _parent: null,
+      _ref: null
+    };
+    this.create = opts => this.store._createInternalModel(assign({}, this.defaults, opts));
+  }
+});
+
+test('create succeeds', function(assert) {
+  let internal = this.create();
+  assert.ok(internal);
+
+  let model = internal.model(true);
+  assert.ok(Duck.detectInstance(model));
+  assert.equal(model.get('ok'), true);
+  assert.equal(model.get('store'), this.store);
+  assert.equal(model.get('database'), this.db);
+});
