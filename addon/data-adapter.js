7c845260b5258d3256424981d17323bab8baef26
ampatspell <ampatspell@gmail.com>
2017-11-24 01:39:52 +0200

    don't include models with `-` prefix

--- addon/data-adapter.js
+++ addon/data-adapter.js
@@ -1,14 +1,25 @@
 import DataAdapter from '@ember/debug/data-adapter';
 import { inject as service } from '@ember/service';
-import { get } from '@ember/object';
+import { get, set } from '@ember/object';
+import { typeOf } from '@ember/utils';
 import Model from './document/model';
 import Models from './document/models';
 
 // https://github.com/emberjs/data/blob/master/addon/-private/system/debug/debug-adapter.js
 
+const __documents_data_adapter_name = '__documents_data_adapter_name';
+
 export default DataAdapter.extend({
 
   stores: service(),
+
+  _nameToClass(name) {
+    let result = this._super(...arguments);
+    if(typeOf(result) === 'class') {
+      set(result, __documents_data_adapter_name, name);
+    }
+    return result;
+  },
 
   detect(typeClass) {
     if(typeClass === Model) {
@@ -17,12 +28,17 @@ export default DataAdapter.extend({
     if(typeClass === Models) {
       return;
     }
-    if(Model.detect(typeClass)) {
-      return true;
+    if(!Model.detect(typeClass) && !Models.detect(typeClass)) {
+      return;
     }
-    if(Models.detect(typeClass)) {
-      return true;
+    let name = get(typeClass, __documents_data_adapter_name);
+    if(name) {
+      let components = name.split('/');
+      if(components[components.length - 1].indexOf('-') === 0) {
+        return;
+      }
     }
+    return true;
   },
 
   columnsForType(typeClass) {
