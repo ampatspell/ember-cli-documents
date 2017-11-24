34322c28452405bd3de7d62ba3813da6456a74a9
ampatspell <ampatspell@gmail.com>
2017-11-24 11:58:30 +0200

    remove session changeset

--- tests/dummy/app/components/ui-block/session/sign-in/component.js
+++ tests/dummy/app/components/ui-block/session/sign-in/component.js
@@ -1,17 +1,17 @@
 import Component from '@ember/component';
-import { changeset } from 'dummy/models/session';
 import layout from './template';
+import { readOnly } from '@ember/object/computed';
 
 export default Component.extend({
   classNameBindings: [ ':ui-block', ':session-sign-in' ],
   layout,
 
-  changeset: changeset(),
+  session: readOnly('state.session'),
 
   actions: {
     async enter() {
       try {
-        await this.get('changeset').commit();
+        await this.get('session').save();
         this.attrs.didSignIn && this.attrs.didSignIn();
       } catch(err) {
         if(err.error !== 'unauthorized') {
--- tests/dummy/app/models/session.js
+++ tests/dummy/app/models/session.js
@@ -1,21 +1,30 @@
-import { Model, model } from 'documents';
-import { readOnly } from '@ember/object/computed';
+import { Model } from 'documents';
+import { readOnly, alias } from '@ember/object/computed';
 
-export const changeset = () => model({
-  create() {
-    return {
-      type: 'session/changeset'
-    };
-  }
-});
+const session = name => alias(`session.${name}`);
 
 export default Model.extend({
 
   session: readOnly('store.session'),
 
+  isAuthenticated: session('isAuthenticated'),
+  isSaving:        session('isSaving'),
+  isError:         session('isError'),
+  name:            session('name'),
+  password:        session('password'),
+  error:           session('error'),
+
   async restore() {
     await this.get('session').restore();
     return { ok: true };
+  },
+
+  async save() {
+    await this.get('session').save();
+  },
+
+  async delete() {
+    await this.get('session').delete();
   }
 
 });
