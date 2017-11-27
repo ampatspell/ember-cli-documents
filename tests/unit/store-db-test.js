a4e7c35e8e5443ad1c8f53b156232e358a46ba4b
ampatspell <ampatspell@gmail.com>
2017-11-27 02:25:03 +0200

    unittests updated

--- tests/unit/store-db-test.js
+++ tests/unit/store-db-test.js
@@ -1,22 +1,22 @@
 import module from '../helpers/module-for-db';
 import { test } from '../helpers/qunit';
 
-module('store-db');
+module('store-id');
 
 test('db', function(assert) {
-  assert.ok(this.store.get('db'));
-  assert.ok(this.store.get('db').toString().includes('documents:databases'));
+  assert.ok(this.store.get('id'));
+  assert.ok(this.store.get('id').toString().includes('documents:store/id'));
 });
 
 test('lookup existing database', function(assert) {
-  let db = this.store.get('db.ember-cli-documents');
+  let db = this.store.get('id.ember-cli-documents');
   assert.ok(db);
   assert.ok(db === this.db);
 });
 
 test('lookup database', function(assert) {
-  let db1 = this.store.get('db.first');
-  let db2 = this.store.get('db.second');
+  let db1 = this.store.get('id.first');
+  let db2 = this.store.get('id.second');
   assert.ok(db1);
   assert.ok(db2);
   assert.ok(db1 !== db2);
