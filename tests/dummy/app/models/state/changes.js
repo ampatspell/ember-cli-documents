bed810c4b200957e9fdd0446eb9a55930c06569f
ampatspell <ampatspell@gmail.com>
2017-11-27 03:22:36 +0200

    keep changes feed around

--- tests/dummy/app/models/state/changes.js
+++ tests/dummy/app/models/state/changes.js
@@ -3,8 +3,11 @@ import LifecycleMixin from '../-lifecycle-mixin';
 
 export default Model.extend(LifecycleMixin, {
 
+  feed: null,
+
   async start() {
     let changes = this.get('database').changes({ feed: [ 'continuous', 'event-source', 'long-polling' ] });
+    this.set('feed', changes);
     changes.start();
     return { ok: true };
   }
