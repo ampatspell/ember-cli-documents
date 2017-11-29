a7b25f813d7a3efa8699457c1e1d70560550a355
ampatspell <ampatspell@gmail.com>
2017-11-29 03:59:27 +0200

    dummy (use embedded models source)

--- tests/dummy/app/components/ui-route/index/component.js
+++ tests/dummy/app/components/ui-route/index/component.js
@@ -1,39 +1,11 @@
 import Component from '@ember/component';
 import layout from './template';
-import { find, models } from 'documents';
+import { documents } from 'dummy/models/documents';
 
 export default Component.extend({
   classNameBindings: [ ':ui-route', ':index' ],
   layout,
 
-  docs: find({
-    database: 'state.database',
-    query() {
-      return { all: true }
-    },
-    matches() {
-      return true;
-    }
-  }),
-
-  models: models({
-    owner: [ 'docs' ],
-    create(owner) {
-      let source = owner.get('docs');
-      return {
-        type: 'documents',
-        source
-      };
-    },
-    model: {
-      observe: [],
-      create(doc) {
-        return {
-          type: 'document',
-          props: { doc }
-        };
-      }
-    }
-  }),
+  documents: documents()
 
 });
--- tests/dummy/app/models/documents.js
+++ tests/dummy/app/models/documents.js
@@ -1,5 +1,35 @@
-import { Models } from 'documents';
+import { Models, find, models, database } from 'documents';
 import LifecycleMixin from './-lifecycle-mixin';
 
+export const documents = () => models({
+  create() {
+    return {
+      type: 'documents'
+    };
+  }
+});
+
 export default Models.extend(LifecycleMixin, {
+
+  database: database('remote', 'main'),
+
+  source: find({
+    query() {
+      return { all: true }
+    },
+    matches() {
+      return true;
+    }
+  }),
+
+  model: {
+    observe: [],
+    create(doc, documents) {
+      return {
+        type: 'document',
+        props: { documents, doc }
+      };
+    }
+  }
+
 });
