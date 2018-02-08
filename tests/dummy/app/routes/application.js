b12d7618a3b64a0044cf1b38f4e323e3465afd50
Arnis Vuskans <ampatspell@gmail.com>
2018-02-08 08:12:34 +0200

    remove tests

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -2,12 +2,12 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
 
-  async beforeModel() {
-    // let state = this.get('state');
-    // let results = await state.restore();
-    // if(results.setup.needed) {
-    //   this.transitionTo('setup');
-    // }
-  }
+  // async beforeModel() {
+  //   let state = this.get('state');
+  //   let results = await state.restore();
+  //   if(results.setup.needed) {
+  //     this.transitionTo('setup');
+  //   }
+  // }
 
 });
