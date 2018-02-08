b76f704d218d328fe5f5ccd1313b3b62a5365bf4
Arnis Vuskans <ampatspell@gmail.com>
2018-02-08 07:11:41 +0200

    remove environment.documents.*

--- config/environment.js
+++ config/environment.js
@@ -1,13 +1,7 @@
 /*jshint node:true*/
 'use strict';
 
-let pkg = require('../package');
-
 module.exports = function(/* environment, appConfig */) {
   return {
-    documents: {
-      name: pkg.name,
-      version: pkg.version
-    }
   };
 };
